5e4fb865311d091ab28675c1efd8c33a37768bb3
FarhanKurnia <itsfarhankurnia@.com>
2022-09-13 04:01:56 +0700

    add seeder for user

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([[
+            'name'=> 'farhan',
+            'email'=> 'user1@gmail.com',
+            'password'=>app('hash')->make('rahasia'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ],
+        [
+            'name'=> 'kurnia',
+            'email'=> 'user2@gmail.com',
+            'password'=>app('hash')->make('rahasia'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]]);
+    }
+}
