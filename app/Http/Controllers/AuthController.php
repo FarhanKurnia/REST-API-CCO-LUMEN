164cf1955a9adbbb1b1cdf0f7809525a9718174f
FarhanKurnia <itsfarhankurnia@gmail.com>
2022-09-14 10:21:23 +0700

    Fixing Routes and implement auth JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Http\Request;
 use App\Models\User;
 
 class AuthController extends Controller
 {
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     /**
      * Register Function
      *
@@ -86,81 +92,5 @@ class AuthController extends Controller
             'message' => 'Sorry, the user cannot be logged out'
         ]);
       }
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,32 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+// API route group
+$router->group(['prefix' => 'api'], function () use ($router) {
+    // Matches "/api/register
+    $router->post('register', 'AuthController@register');
+    // Matches "/api/login
+    $router->post('login', 'AuthController@login');
 
-$router->post('register', 'AuthController@register');
-$router->post('login', 'AuthController@login');
+    // Matches "/api/profile
+    $router->get('profile', 'UserController@profile');
+
+    // Matches "/api/users/1 
+    //get one user by id
+    $router->get('users/{id}', 'UserController@singleUser');
+
+    // Matches "/api/users
+    $router->get('users', 'UserController@allUsers');
+
+    $router->get('bts/{id}','BtsController@show');
+    $router->get('bts','BtsController@index');
+});
+// $router->post('register', 'AuthController@register');
+// $router->post('login', 'AuthController@login');
 $router->group(['middleware' => 'auth'], function () use ($router) {
     $router->get('logout', 'AuthController@logout');
   });
 
-$router->get('bts/{id}','BtsController@show');
-$router->get('bts','BtsController@index');
+
 
 
