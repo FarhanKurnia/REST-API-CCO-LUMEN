3483f443419f95d13e0a07923c45690a9cc37312
farhankurnia <itsfarhankurnia@gmail.com>
2022-10-16 00:23:24 +0700

    Fixing Cors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $request = app('request');
+        if ($request->isMethod('OPTIONS')){
+            app()->options($request->path(), function() { return response('', 200); });
+        }
     }
 
     public function boot()
