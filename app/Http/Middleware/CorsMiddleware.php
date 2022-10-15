85ae738d6f4ade693b64232ff0b3b9b50ca1610e
farhankurnia <itsfarhankurnia@gmail.com>
2022-10-16 00:40:49 +0700

    try to fix cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,23 +10,48 @@ class CorsMiddleware
  * @param  \Closure  $next
  * @return mixed
  */
-public function handle($request, Closure $next)
-{
-    //Intercepts OPTIONS requests
-    if ($request->isMethod('OPTIONS')) {
-        $response = response('', 200);
-    } else {
-        // Pass the request to the next middleware
+
+    public function handle($request, Closure $next)
+    {
+        // cors baru
+        $headers = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age'           => '86400',
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+        ];
+
+        if ($request->isMethod('OPTIONS'))
+        {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
+
         $response = $next($request);
+        foreach($headers as $key => $value)
+        {
+            $response->header($key, $value);
+        }
+
+        return $response;
     }
 
-    // Adds headers to the response
-    $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
-    $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-    $response->header('Access-Control-Allow-Origin', '*');
-    $response->header('Access-Control-Expose-Headers', 'Location');
+    // Cors lama
+    // //Intercepts OPTIONS requests
+    // if ($request->isMethod('OPTIONS')) {
+    //     $response = response('', 200);
+    // } else {
+    //     // Pass the request to the next middleware
+    //     $response = $next($request);
+    // }
 
-    // Sends it
-    return $response;
-}
+    // // Adds headers to the response
+    // $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
+    // $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
+    // $response->header('Access-Control-Allow-Origin', '*');
+    // $response->header('Access-Control-Expose-Headers', 'Location');
+
+    // // Sends it
+    // return $response;
+
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -95,7 +95,9 @@ $app->routeMiddleware([
 |
 */
 
-$app->register(App\Providers\AppServiceProvider::class);
+// $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\TimeZoneServiceProvider::class);
+$app->register(App\Providers\CatchAllOptionsRequestsProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
